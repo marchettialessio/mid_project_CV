d64e40a5f27661fb163410095d2fe9de26010956
nicolospuri <nicky.spuri@gmail.com>
2026-04-22 23:14:27 +0200

    use SIFT to detect features and Lukas-Kanade to detect motion

--- src/prova1.cpp
+++ src/prova1.cpp
@@ -1,31 +1,10 @@
 #include <opencv2/opencv.hpp>
 #include <filesystem>
 
-int main(int argc, char** argv) {
-    if (argc < 2) {
-        std::cerr << "You have to specify the folder" << argv[0];
-        return -1;
-    }
+void opticalFLow(std::vector<std::string> images) {
+    // TODO: tunare per ogni immagine e in base al detector utilizzato, ognuna ha la sua
+    constexpr float minMotion = 0.1f;
 
-    std::string folderName = argv[1];
-
-    std::string folderPath = "../../resources/data/" + folderName;   // your folder containing frames
-    std::vector<std::string> imagePaths;
-
-    // Read all image files from directory
-    for (const auto& entry : std::filesystem::directory_iterator(folderPath)) {
-        imagePaths.push_back(entry.path().string());
-    }
-
-    // Sort file names so frames are processed in order
-    std::sort(imagePaths.begin(), imagePaths.end());
-
-    if (imagePaths.empty()) {
-        std::cerr << "No images found in folder: " << folderPath << std::endl;
-        return -1;
-    }
-
-    const float minMotion = 0.1f;
     // Create some random colors to draw the moving features
     std::vector<cv::Scalar> colors;
     cv::RNG rng;
@@ -37,71 +16,71 @@ int main(int argc, char** argv) {
         colors.push_back(cv::Scalar(r,g,b));
     }
 
-    cv::Ptr<cv::SIFT> detector = cv::SIFT::create();
-    std::vector<cv::KeyPoint> keypoints;
-
-    cv::Mat frame = cv::imread(imagePaths[0]);
+    cv::Mat frame = cv::imread(images[0]);
     cv::Mat grayFrame;
     cv::cvtColor(frame, grayFrame, cv::COLOR_BGR2GRAY);
 
-    std::cout << "Processing: " << imagePaths[0] << std::endl;
+    std::cout << "Processing: " << images[0] << std::endl;
 
-    // Features detection with SIFT
+    std::vector<cv::Point2f> currKP;
+
+
+    // Detect keypoints using SIFT
+    cv::Ptr<cv::SIFT> detector = cv::SIFT::create();
+    std::vector<cv::KeyPoint> keypoints;
+    // TODO: impostare la mask in base ad ogni immagine
+    // TODO: tunare bene per tutte le immagini (in particolare bird e squirrel)
+    detector->detect(frame, keypoints);
+    cv::KeyPoint::convert(keypoints, currKP);
+
+    std::cout << "Keypoints size: " << currKP.size() << std::endl;
+
+
     /*
-    detector->detect(grayFrame, keypoints);
-    cv::Mat output;
-    cv::drawKeypoints(frame, keypoints, output);
-    */
-    //cv::imshow("Features0", output);
-
-    std::vector<cv::Point2f> p0, p1, firstFeatures;
+    // Detect features using goodFeaturesToTrack
     // Take first frame and find corners in it
-    goodFeaturesToTrack(grayFrame, p0, 100, 0.3, 7, cv::Mat(), 7, false, 0.04);
-    firstFeatures = p0;
+    // TODO: impostare la mask in base ad ogni immagine
+    // TODO: tunare bene per tutte le immagini (in particolare bird e squirrel)
+    goodFeaturesToTrack(grayFrame, currKP, 100, 0.3, 7, cv::Mat(), 7, false, 0.04);
+
+    std::cout << "Keypoints size: " << currKP.size() << std::endl;
+    */
     // Create a mask image for drawing purposes
     cv::Mat mask = cv::Mat::zeros(frame.size(), frame.type());
+    std::vector<cv::Point2f> newKP;
 
     // Read and process all images
-    for (size_t i = 1; i < imagePaths.size(); i++) {
+    for (size_t i = 1; i < images.size(); i++) {
         cv::Mat oldFrame = frame.clone();
         cv::Mat oldGray = grayFrame.clone();
-        frame = cv::imread(imagePaths[i]);
+        frame = cv::imread(images[i]);
         cv::cvtColor(frame, grayFrame, cv::COLOR_BGR2GRAY);
 
         if (frame.empty()) {
-            std::cerr << "Could not read image: " << imagePaths[i] << std::endl;
+            std::cerr << "Could not read image: " << images[i] << std::endl;
             continue;
         }
 
-        std::cout << "Processing: " << imagePaths[i] << std::endl;
-
-        // Features detection with sift
-        /*
-        detector->detect(grayFrame, keypoints);
-        cv::drawKeypoints(frame, keypoints, output);
-        */
-        //cv::imshow("Features" + std::to_string(i), output);
+        std::cout << "Processing: " << images[i] << std::endl;
 
         // Optical flow
         std::vector<uchar> status;
         std::vector<float> err;
         cv::TermCriteria criteria = cv::TermCriteria((cv::TermCriteria::COUNT) + (cv::TermCriteria::EPS), 10, 0.03);
-        calcOpticalFlowPyrLK(oldGray, grayFrame, p0, p1, status, err, cv::Size(15,15), 2, criteria);
+        calcOpticalFlowPyrLK(oldGray, grayFrame, currKP, newKP, status, err, cv::Size(15,15), 2, criteria);
 
-        std::vector<cv::Point2f> good_new, moving_pts;
+        std::vector<cv::Point2f> moving_pts;
 
-        for (uint j = 0; j < p0.size(); j++) {
+        for (uint j = 0; j < currKP.size(); j++) {
             // Select good points, status == 1 if flow has been found
-            if (status[j] == 1) {
-                good_new.push_back(p1[j]);
-
-                float d = cv::norm(p1[j] - p0[j]);  // Euclidean distance
+            if (status[j] == 1) {   // TODO: keep track of every keypoint to compare the various vectors of keypoints and find the object in the first image
+                float d = cv::norm(newKP[j] - currKP[j]);  // Euclidean distance
 
                 if (d > minMotion) {
-                    moving_pts.push_back(p1[j]);
+                    moving_pts.push_back(newKP[j]);
                     // draw the tracks
-                    cv::line(mask,p1[j], p0[j], colors[j], 2);
-                    circle(frame, p1[j], 5, colors[j], -1);
+                    cv::line(mask,newKP[j], currKP[j], colors[j], 2);
+                    circle(frame, newKP[j], 5, colors[j], -1);
                 }
             }
         }
@@ -119,8 +98,37 @@ int main(int argc, char** argv) {
             break;
 
         // Update the previous frame and previous points
-        p0 = good_new;
+        currKP = moving_pts;
+
+        std::cout << "Keypoints size: " << currKP.size() << std::endl;
     }
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr << "You have to specify the folder" << argv[0];
+        return -1;
+    }
+
+    std::string folderName = argv[1];
+
+    std::string folderPath = "../../resources/data/" + folderName;   // your folder containing frames
+    std::vector<std::string> images;
+
+    // Read all image files from directory
+    for (const auto& entry : std::filesystem::directory_iterator(folderPath)) {
+        images.push_back(entry.path().string());
+    }
+
+    // Sort file names so frames are processed in order
+    std::sort(images.begin(), images.end());
+
+    if (images.empty()) {
+        std::cerr << "No images found in folder: " << folderPath << std::endl;
+        return -1;
+    }
+
+    opticalFLow(images);
 
     return 0;
 }
