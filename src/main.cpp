a7b989ca52077fbb09d9ea24ae2267f887bd72b0
alessioMarchettiMadlab <alessio.marchetti@madlab.biz>
2026-04-22 14:34:54 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/features2d.hpp>
+//prova
 
 int main() {
     cv::Mat img = cv::imread("../resources/data/frog/0000.png");
