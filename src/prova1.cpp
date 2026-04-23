33cf3ae8ffd458b11f19c966140e1bd940e4b8fd
nicolospuri <nicky.spuri@gmail.com>
2026-04-23 14:21:20 +0200

    aggiustato metodo per aggiornare il vettore dei keypoint iniziali

--- src/prova1.cpp
+++ src/prova1.cpp
@@ -2,8 +2,14 @@
 #include <filesystem>
 
 void opticalFLow(std::vector<std::string> images) {
-    // TODO: tunare per ogni immagine e in base al detector utilizzato, ognuna ha la sua
-    constexpr float minMotion = 0.1f;
+    // TODO: tunare per ogni immagine e in base al detector utilizzato, ognuna ha la sua (forse per robustezza ne serve una unica, non deve dipendere dalle immagini)
+    // TODO: al massimo si può mettere un tot di valori e in base ai risultati confrontati con le label scegliere il migliore
+    constexpr float minMotion = 0.005f;
+    // bird
+    // car 0.4f SIFT
+    // frog 0.005f SIFT
+    // sheep 0.05
+    // squirrel
 
     // Create some random colors to draw the moving features
     std::vector<cv::Scalar> colors;
@@ -23,12 +29,15 @@ void opticalFLow(std::vector<std::string> images) {
 
     std::cout << "Processing: " << images[0] << std::endl;
 
+    // TODO: impostare la mask in base ad ogni immagine (forse, non so se per essere robusto non deve dipendere dall'immagine)
+    // TODO: tunare bene per tutte le immagini e provare altri detector (in particolare bird e squirrel)
+    // TODO: al massimo si può mettere un tot di detector e threshold e in base ai risultati confrontati con le label scegliere il migliore
+
+
     // Detect keypoints using SIFT
     std::vector<cv::Point2f> initialKP;
     cv::Ptr<cv::SIFT> detector = cv::SIFT::create();
     std::vector<cv::KeyPoint> keypoints;
-    // TODO: impostare la mask in base ad ogni immagine
-    // TODO: tunare bene per tutte le immagini (in particolare bird e squirrel)
     detector->detect(initialGray, keypoints);
     cv::KeyPoint::convert(keypoints, initialKP);    // Convert to point in 2D
 
@@ -44,14 +53,16 @@ void opticalFLow(std::vector<std::string> images) {
 
     // Create a mask image for drawing purposes
     cv::Mat mask = cv::Mat::zeros(initialFrame.size(), initialFrame.type());
+    // Set frame and keypoints for the next iteration
     cv::Mat oldGray = initialGray;
     std::vector<cv::Point2f> currKP = initialKP;
+    std::vector<cv::Point2f>* usefulKP = &initialKP;    // Pointer to vector of useful keypoints to draw the rectangle
 
     // TODO: se KP.size va a 0 break
 
-    // Read and process all images
+    // Read and process images
     for (size_t i = 1; i < images.size(); i++) {
-        // Read frame i and get grayscale
+        // Read frame 'i' and get grayscale
         cv::Mat frame = cv::imread(images[i]);
         cv::Mat grayFrame;
         cv::cvtColor(frame, grayFrame, cv::COLOR_BGR2GRAY);
@@ -72,26 +83,23 @@ void opticalFLow(std::vector<std::string> images) {
         calcOpticalFlowPyrLK(oldGray, grayFrame, currKP, newKP, status, err, cv::Size(15,15), 2, criteria);
 
         std::vector<cv::Point2f> moving_pts, good_new;
+        std::vector<cv::Point2f> currInitialKP = *usefulKP;
 
         // TODO: sistemare erase, trovare altro modo per ottenere un nuovo vettore di keypoints iniziali con possibile dimensione minore
         // Scan the vectors of keypoints
-        for (uint j = currKP.size()-1; j > 0; j--) { // TODO: salta l'iterazione 0
+        for (uint j = 0; j < currKP.size(); j++) {
             // Select good points, status == 1 if flow has been found
             if (status[j] == 1) {
                 float d = cv::norm(newKP[j] - currKP[j]);  // Euclidean distance
-
+                // TODO: impostare che se una feature si è mossa un tot di volte è definitiva, perché ad esempio nella frog alcune feature finiscono fuori dall'immagine e vengono perse
                 if (d >= minMotion) {    // if the point moved more or equal than minMotion -> track the flow
-                    moving_pts.push_back(initialKP[j]);
+                    moving_pts.push_back(currInitialKP[j]);
                     good_new.push_back(newKP[j]);
 
                     // Draw the tracks
-                    cv::line(mask,newKP[j], currKP[j], colors[j], 2);
-                    circle(frame, newKP[j], 5, colors[j], -1);
-                } else {
-                    initialKP.erase(initialKP.begin() + j);
+                    cv::line(mask,newKP[j], currKP[j], colors[j % colors.size()], 2);
+                    circle(frame, newKP[j], 5, colors[j % colors.size()], -1);
                 }
-            } else {
-                initialKP.erase(initialKP.begin() + j);
             }
         }
         // Put in img frame and mask to draw the new flow
@@ -100,6 +108,7 @@ void opticalFLow(std::vector<std::string> images) {
 
         // Draw on a new image the rectangle updated
         cv::Mat newInitialFrame = initialFrame.clone();
+        *usefulKP = moving_pts;
         cv::Rect box = cv::boundingRect(moving_pts);
         // Clamping at the borders, intersection between box and the rectangle representing the whole image
         // to avoid getting a box out of the image
